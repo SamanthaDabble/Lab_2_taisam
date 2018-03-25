34dcab393b303af27a829e8b4a44dfc3e1c0f823
Samantha <taisam@dunwoody.edu>
2018-03-25 09:28:24 -0500

    adding files and changes

---
+++ index.js
@@ -0,0 +1,18 @@
+import { createServer } from "https";
+
+const http = require("http");
+const fs = require("fs");
+const data = require("./top_2018_movies.json");
+
+//Create a function that lists all the movies
+
+//Create a fuction that lists the movies that grossed above 20 million and genre is action
+
+//Create a function that lists the movies that are rated "PG-13" and number of tickets sold is between 1 and 5 million
+
+//Create a function that sorts the movies based on "distributor"
+
+//createServer();
+//listen();
+
+//readFile();
